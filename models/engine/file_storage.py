2706695afac411c6b753d7fd90bf7096cf8fb6cf
davidajimati <ajimatidavid@gmail.com>
2023-02-07 23:33:25 +0100

    #FEAT: UPDATED: filestorage.py & base_model.py & .gitignore

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,19 +8,26 @@ from os import path
 
 
 class FileStorage:
-    __file_path = "database.json"
-    __objects = {}
+    ''' Class declaration '''
+
+    def __init__(self, filepath):
+        self.__file_path = filepath
+        self.__objects = {}
 
     def all(self):
+        ''' returns the objects dictionary '''
         return self.__objects
 
     def new(self, obj):
+        ''' creates a new instance '''
         self.__objects["{}.{}".format(self.__class__.__name__, self.id)] = obj
 
     def save(self):
+        ''' saves an objects into a JSON file '''
         with open(self.__file_path, 'w') as file:
             json.dump(self.__objects, file)
 
     def reload(self):
+        ''' reloads objects from a JSON file '''
         if path.exists(self.__file_path):
             self.__objects = json.load(self.__file_path)

a5720492a659971f7dc536646b6211fb0f279000
davidajimati <ajimatidavid@gmail.com>
2023-02-07 11:54:29 +0100

    #FEAT: basemode class | test folder  created

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''
+This is the Baseclass for all other classes in the
+AirBnB clone project
+'''
+
+
+class BaseModel:
+    ''' THe BaseModel class defined here'''
+
+    def __init__(self, id)

1bd0597280471a4ccf987fbde01b679405cbc47e
Q-codes <franciskwafe@gmail.com>
2023-02-08 08:08:33 -0800

    imported more model modules

--- console.py
+++ console.py
@@ -3,6 +3,13 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     intro = 'Welcome to the AirBNB clone console. Type help or ? to list commands.\n'

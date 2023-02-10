741e52ed831e75fdd1e1dfb26dcb403f81899299
davidajimati <ajimatidavid@gmail.com>
2023-02-10 20:56:04 +0100

    Created the Destroy, do_all, and do_update methods to the console module

--- console.py
+++ console.py
@@ -31,6 +31,7 @@ class HBNBCommand(cmd.Cmd):
     Class definition for the command line processor
     """
     prompt = '(hbnb) '
+    all_classes = [BaseModel, User, State, Review, Place, City, Amenity]
 
     def do_quit(self, line):
         '''Quit command to exit the program
@@ -60,16 +61,99 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
 
     def do_show(self, args):
+        """ Returns the string representation an instance """
+
+        target = argv[0]
         args = args.split()
-        if len(args) != 1:
+        if len(args) < 1:
             print("** class name missing **")
+
+        elif len(args) < 2:
+            print("** instance id missing **")
+
+        elif args[0] not in self.all_classes:
+            print("** class doesn't exist **")
+
         else:
-            try:
-                latest = eval("{}()".format(args[0]))
+            data = storage.all().get(target + '.' + args[1])
+            if data is None:
+                print("** no instance found **")
+            else:
+                print(data)
+
+    def do_destroy(self, args):
+        """Deletes an instance"""
+
+        args = args.split()
+        target = args[0]
+        iid = args[1]
+        if len(args) < 1:
+            print("** class name missing **")
+
+        elif len(args) < 2:
+            print("** instance id missing **")
+
+        elif args[0] not in self.all_classes:
+            print("** class doesn't exist **")
+
+        else:
+            data = storage.all().get(target + '.' + iid)
+            if data is None:
+                print("** no instance found **")
+            else:
+                del data
                 storage.save()
-                print(latest.id)
-            except Exception:
+
+    def do_all(self, args):
+        args = args.split()
+        target = args[0]
+        if len(args) == 0:
+            objects = storage.all()
+            for item in objects:
+                print(item)
+        else:
+            out = []
+            idx = 0
+            if target in self.all_classes:
+                objects = storage.all[target]
+                for item in objects.values():
+                    out[idx] = item
+                    idx += 1
+                print(out)
+            else:
                 print("** class doesn't exist **")
+
+    def do_update(self, args):
+        cls_n = args[0]
+        iid = args[1]
+        attr_n = args[2]
+        new_val = args[3]
+
+        if len(args) < 1:
+            print("** class name missing **")
+
+        elif iid < 2:
+            print("** instance id missing **")
+
+        elif cls_n not in self.all_classes:
+            print("** class doesn't exist **")
+
+        elif attr_n not in self.all_classes:
+            print("** attribute name missing **")
+
+        elif new_val not in self.all_classes:
+            print("** value missing **")
+
+        else:
+            data = storage.all().get(cls_n + '.' + iid)
+            if data is None:
+                print("** no instance found **")
+            else:
+                object = storage.all([cls_n])
+                for k, v in object.items():
+                    if k == attr_n:
+                        object[attr_n] = new_val
+        storage.save()
 
 
 if __name__ == "__main__":
